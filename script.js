fa8654d615ba9ee627a6276a4c9603f9437b53e3
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2023-06-14 18:42:52 +0200

    Récupération de la data pour les caroussels OK.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const urlImdbScore = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score";
 const urlImdbScore7films = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=7";
+const urlImdbScore7filmsCrimeCategory = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=7&genre=crime";
+const urlImdbScore7filmsMysteryCategory = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=7&genre=mystery";
+const urlImdbScore7filmsSportCategory = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=7&genre=sport";
 const urlErrorMessage = "Problème dans l'URL fourni ! ";
 const jsonErrorMessage = "Problème dans la conversion en JSON ! ";
 const idLocationErrorMessage = "Problème de la localisation de l'ID dans le code HTML ! ";
-const bestFilmsH1Text = "Films les mieux notés";
+
 
 // Fonction qui récupère les données d'une URL donnée et les renvoie sous format JSON
 async function getData(url) {
@@ -17,79 +20,171 @@ async function getData(url) {
     return response.json();
 }
 
-/* Fonction qui crée un élément HTML ayant les paramètres suivants :
-idLocationInHtml = ID de la balise dans laquelle on souhaite créer l'élément.
-elementIdToCreate = ID de l'élément à créer.
-content = Contenu de l'élément à créer.
+/* Fonction qui crée une balise HTML ayant les paramètres suivants :
+typeToCreate = Type de la balise.
+textContent = Contenu texte de la balise.
+className = Classe de la balise(ignoré si nul).
+idName = ID de la balise(ignoré si nul).
 */
-async function createHtmlElement(idLocationInHtml, elementIdToCreate, content) {
-    const location = document.getElementById(idLocationInHtml);
-    const element = await document.createElement(elementIdToCreate);
-    element.textContent = content; 
-    if (!location) {
-        throw new Error (idLocationErrorMessage);
+function createHtmlElement(typeToCreate, textContent, className = null, idName = null) {
+    const element = document.createElement(typeToCreate);
+    element.textContent = textContent; 
+    if (className !==  null) { 
+        element.className = className;
     }
-    location.appendChild(element);
+    if (idName !==  null) { 
+        element.id = idName;
+    }
+
+    return element;
 }
 
+/* Fonction qui crée une balise HTML ayant les paramètres suivants :
+typeToCreate = Type de la balise.
+textContent = Contenu texte de la balise.
+dataAction = Action déclenchée par la balise.
+className = Classe de la balise(ignoré si nul).
+idName = ID de la balise(ignoré si nul).
+*/
+function createHtmlElementWithDataAction(typeToCreate, textContent, dataAction, className = null, idName = null) {
+    const element = document.createElement(typeToCreate);
+    element.textContent = textContent; 
+    if (className !==  null) { 
+        element.className = className;
+    }
+    if (idName !==  null) {
+        element.id = idName;
+    }
+    
+    element.dataset.action = dataAction;
+
+    return element;
+}
+
+/* Fonction qui crée une balise HTML ayant les paramètres suivants :
+typeToCreate = Type de la balise.
+textContent = Contenu texte de la balise.
+dataTarget = Data ciblée par la balise.
+className = Classe de la balise(ignoré si nul).
+idName = ID de la balise(ignoré si nul).
+*/
+function createHtmlElementWithDataTarget(typeToCreate, textContent, dataTarget, className = null, idName = null) {
+    const element = document.createElement(typeToCreate);
+    element.textContent = textContent; 
+    if (className !==  null) { 
+        element.className = className;
+    }
+    if (idName !==  null) {
+        element.id = idName;
+    }
+    
+    element.dataset.target = dataTarget;
+
+    return element;
+}
+
+
 /* Fonction qui crée une balise HTML image ayant les paramètres suivants :
-idLocationInHtml = ID de la balise dans laquelle on souhaite créer l'image.
 imgUrl = URL de l'image.
 imgAlt = Texte alternatif de l'image.
 */
-async function createHtmlImg(idLocationInHtml, imgUrl, imgAlt) {
-    const location = document.getElementById(idLocationInHtml);
+function createHtmlImg(imgUrl, imgAlt) {
     const img = document.createElement("img");
     img.src = imgUrl;
     img.alt = imgAlt + " Cover Image";
-    
-    if (!location) {
-        throw new Error (idLocationErrorMessage);
-    }
-    location.appendChild(img);
+    return img;
 }
+
+
+/* Fonction qui crée un caroussel selon les paramètres suivants :
+sectionId = ID de la balise dans laquelle on souhaite créer le caroussel.
+carouselTitle = Titre du caroussel qui sera affiché à l'utilisateur.
+data = données à partir desquels se crée le caroussel.
+*/
+
+function carousel(sectionId, carouselTitle, data) {
+    const sectionTitle = sectionId;
+    const sectionLocation = document.getElementById(sectionTitle);
+    const dataDetails = data.results;
+    const h1 = createHtmlElement("h1", carouselTitle);
+    sectionLocation.appendChild(h1);
+    const buttonDiv = createHtmlElement("div", "", "button-carousel");
+    sectionLocation.appendChild(buttonDiv);
+    const buttonL = createHtmlElementWithDataAction("button", "L", "slideLeft");
+    buttonDiv.appendChild(buttonL);
+    const buttonR = createHtmlElementWithDataAction("button", "R", "slideRight");
+    buttonDiv.appendChild(buttonR);
+    const carouselUl = createHtmlElementWithDataTarget("ul", "", "carousel", "carousel");
+    sectionLocation.appendChild(carouselUl);
+    console.log(dataDetails);
+    for (let film of dataDetails) {
+        const filmCard = createHtmlElementWithDataTarget("li", "", "card", "card");
+        const filmImg = createHtmlImg(film.image_url, film.title);
+        filmCard.appendChild(filmImg);
+        carouselUl.appendChild(filmCard);
+    }
+}
+
 
 // Récupération et affichage de la data du meilleur film selon sa note IMDb (best film)
 const bestFilmsPromise = getData(urlImdbScore);
 bestFilmsPromise.then(bestFilms => {
+    const sectionTitle = "best-film";
+    const sectionLocation = document.getElementById(sectionTitle);
     firstFilm = bestFilms.results[0];
     firstFilmTitle = firstFilm.title;
-    createHtmlElement("best-film", "h1", firstFilmTitle);
-    createHtmlImg("best-film", firstFilm.image_url, firstFilm.title);
+    const filmTitle = createHtmlElement("h1", firstFilmTitle);
+    sectionLocation.appendChild(filmTitle);
+    const firstFilmImage = createHtmlImg(firstFilm.image_url, firstFilm.title);
+    sectionLocation.appendChild(firstFilmImage);
 });
 
-// Début de la même chose mais avec les 7 meilleurs films.
+/* Récupération de la data des 7 meilleurs films selon leur note IMDb (best rated films).
+Création d'un caroussel.
+Affichage de la data dans le caroussel.
+*/
+const best7FilmsPromise = getData(urlImdbScore7films);
+const best7FilmsCarousel = best7FilmsPromise.then(data => carousel("best-rated-films", "Les films les mieux notés", data));
+
+// Idem pour la catégorie Crime.
+const best7FilmsCrimeCategoryPromise = getData(urlImdbScore7filmsCrimeCategory);
+const best7FilmsCrimeCategoryCarousel = best7FilmsCrimeCategoryPromise.then(data => carousel("crime-category", "Crime", data));
+
+// Idem pour la catégorie Mystery.
+const best7FilmsMysteryCategoryPromise = getData(urlImdbScore7filmsMysteryCategory);
+const best7FilmsMysteryCategoryCarousel = best7FilmsMysteryCategoryPromise.then(data => carousel("mystery-category", "Mystery", data));
+
+// Idem pour la catégorie Sport.
+const best7FilmsSportCategoryPromise = getData(urlImdbScore7filmsSportCategory);
+const best7FilmsSportCategoryCarousel = best7FilmsSportCategoryPromise.then(data => carousel("sport-category", "Sport", data));
+
+/*
+
 const best7FilmsPromise = getData(urlImdbScore7films);
 best7FilmsPromise.then(best7Films => {
+    const sectionTitle = "best-rated-films";
+    const sectionLocation = document.getElementById(sectionTitle);
+    const bestFilmsH1Text = "Films les mieux notés";
     const allFilms = best7Films.results;
-    const h1 = createHtmlElement("best-rated-films", "h1", bestFilmsH1Text);
-    const buttonDiv = createHtmlElement("best-rated-films", "div", "");
-    buttonDiv.class = "button-carousel";
-    const buttonL = createHtmlElement("button-carousel", "button", "L");
-    buttonL.data-action = "slideLeft";
-    const buttonR = createHtmlElement("button-carousel", "button", "R");
-    buttonL.data-action = "slideLeft";
-    buttonR.data-action = "slideRight";
-    
-    const carouselUl = createHtmlElement("best-rated-films", "ul", "");
-
+    const h1 = createHtmlElement("h1", bestFilmsH1Text);
+    sectionLocation.appendChild(h1);
+    const buttonDiv = createHtmlElement("div", "", "button-carousel");
+    sectionLocation.appendChild(buttonDiv);
+    const buttonL = createHtmlElementWithDataAction("button", "L", "slideLeft");
+    buttonDiv.appendChild(buttonL);
+    const buttonR = createHtmlElementWithDataAction("button", "R", "slideRight");
+    buttonDiv.appendChild(buttonR);
+    const carouselUl = createHtmlElementWithDataTarget("ul", "", "carousel", "carousel");
+    sectionLocation.appendChild(carouselUl);
     console.log(allFilms);
     for (let film of allFilms) {
-
+        const filmCard = createHtmlElementWithDataTarget("li", "", "card", "card");
+        const filmImg = createHtmlImg(film.image_url, film.title);
+        filmCard.appendChild(filmImg);
+        carouselUl.appendChild(filmCard);
     }
 });
 
-
-
-
-
-
-
-
-
-
-
-/*
 
 async function main() {
     const bestFilmsData = await getData(urlImdbScore);
